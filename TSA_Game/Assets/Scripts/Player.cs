2071ee4898f9ce484491c9b9d93bad91e8406d8b
Fishman4821 <102929683+Fisherman4821@users.noreply.github.com>
2025-01-15 09:41:49 -0600

    Worked on gravity (again)

--- TSA_Game/Assets/Scripts/Player.cs
+++ TSA_Game/Assets/Scripts/Player.cs
@@ -11,28 +11,39 @@ public class Player : MonoBehaviour
     public float coyoteTime;
 
     Rigidbody rb;
-    bool grounded;
+    SpriteRenderer sr;
+    bool grounded = false;
 
     bool terminal_velocity()
     {
-        return rb.velocity.y <= -Mathf.Sqrt(2 * rb.mass * gravity / 0.5f);
+        return rb.velocity.y <= -(Mathf.Sqrt(2 * rb.mass * gravity / 0.5f));
     }
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     void FixedUpdate()
     {
-        rb.velocity += new Vector3(Input.GetAxis("Horizontal") * accel, (terminal_velocity() && !grounded) ? -gravity : 0, 0);
+        rb.velocity += new Vector3(Input.GetAxis("Horizontal") * accel, (!grounded) ? ((terminal_velocity()) ? 0 : -gravity) : 0, 0);
 
         if ((Input.GetAxis("Vertical") > 0 || Input.GetKeyDown("space")) && grounded)
         {
             rb.AddForce(new Vector3(0, jumpImpulse, 0), ForceMode.Impulse);
         }
+       
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+        } 
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = false;
+        }
 
-        print(String.Format("vel(x: {0}, y: {1}, z: {2}), grounded: {3}, terminal_velocity: ", rb.velocity.x, rb.velocity.y, rb.velocity.z, grounded));
+        print(String.Format("vel(x: {0}, y: {1}, z: {2}), grounded: {3}, terminal_velocity: {4}, {5}", rb.velocity.x, rb.velocity.y, rb.velocity.z, grounded, terminal_velocity(), Mathf.Sqrt(2 * rb.mass * gravity / 0.5f)));
     }
 
     private void OnCollisionEnter(Collision collision)
