f34373facbd246c570724ce9cb6159d565983f2d
Fishman4821 <102929683+Fisherman4821@users.noreply.github.com>
2025-01-22 10:25:13 -0600

    worked on player movement once more

--- TSA_Game/Assets/Scripts/Player.cs
+++ TSA_Game/Assets/Scripts/Player.cs
@@ -11,7 +11,7 @@ public class Player : MonoBehaviour
     public float coyoteTime = 0.5f;
     public float dragCoefficient = 1f;
 
-    Rigidbody rb;
+    Rigidbody2D rb;
     SpriteRenderer sr;
     bool grounded = false;
     float timeSinceGrounded = 0.0f;
@@ -27,8 +27,8 @@ public class Player : MonoBehaviour
             Cd = Drag Coefficient
             A = 0.5 (area)
         */
-        return new Vector2(1.293f * Mathf.Pow(rb.velocity.x, 2) * Mathf.Sign(rb.velocity.x) * dragCoefficient * 0.5f * 0.5f,
-                           1.293f * Mathf.Pow(rb.velocity.y, 2) * Mathf.Sign(rb.velocity.y) * dragCoefficient * 0.5f * 0.5f);
+        return new Vector2(/*1.293f * */Mathf.Pow(rb.velocity.x, 2) * Mathf.Sign(rb.velocity.x) * dragCoefficient * 0.5f * 0.5f,
+                           /*1.293f * */Mathf.Pow(rb.velocity.y, 2) * Mathf.Sign(rb.velocity.y) * dragCoefficient * 0.5f * 0.5f);
     }
 
     bool terminal_velocity()
@@ -38,18 +38,18 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
-    //float time = 0.0f;
+
     void FixedUpdate()
     {
         Vector2 dragForce = DragForce();
-        rb.velocity += new Vector3(Input.GetAxis("Horizontal") * accel - dragForce.x, ((!grounded) ? -gravity : 0) - dragForce.y, 0);
+        rb.velocity += new Vector2(Input.GetAxis("Horizontal") * accel - dragForce.x, ((!grounded) ? -gravity : 0) - dragForce.y);
 
         if ((Input.GetAxis("Vertical") > 0 || Input.GetKeyDown("space")) && (grounded || timeSinceGrounded > 0.0f) && !jumping)
         {
-            rb.AddForce(new Vector3(0, jumpImpulse, 0), ForceMode.Force);
+            rb.AddForce(new Vector2(0, jumpImpulse), ForceMode2D.Impulse);
             jumping = true;
         }
         if (timeSinceGrounded > 0.0f)
@@ -66,10 +66,10 @@ public class Player : MonoBehaviour
             sr.flipX = false;
         }
 
-        print(String.Format("vel(x: {0}, y: {1}, z: {2}), grounded: {3}, timeSinceGrounded: {4}, jumping: {5}, terminal_velocity: {8}, dragForce(x: {6}, y: {7}), Horizontal: {9}", rb.velocity.x, rb.velocity.y, rb.velocity.z, grounded, timeSinceGrounded, jumping, dragForce.x, dragForce.y, terminal_velocity(), Input.GetAxis("Horizontal")));
+        print(String.Format("vel(x: {0}, y: {1}), grounded: {3}, timeSinceGrounded: {4}, jumping: {5}, terminal_velocity: {8}, dragForce(x: {6}, y: {7}), Horizontal: {9}, Vertical: {2}", rb.velocity.x, rb.velocity.y, Input.GetAxis("Vertical"), grounded, timeSinceGrounded, jumping, dragForce.x, dragForce.y, terminal_velocity(), Input.GetAxis("Horizontal")));
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground") {
             jumping = false;
@@ -77,7 +77,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
