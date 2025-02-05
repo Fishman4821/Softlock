95a8ad3cff28ba8bb71ceb531ef0320c4164cc9d
Redcar005 <williamwoodruff@blazermail.net>
2025-02-05 10:28:06 -0600

    Added Music & Finished Coin Counter

--- TSA_Game/Assets/Scripts/CoinManager.cs
+++ TSA_Game/Assets/Scripts/CoinManager.cs
@@ -8,7 +8,7 @@ public class CoinManager : MonoBehaviour
 {
 
     public int coinCount;
-    public TextMeshPro coinText;
+    public TextMeshProUGUI coinText;
 
     // Start is called before the first frame update
     void Start()
