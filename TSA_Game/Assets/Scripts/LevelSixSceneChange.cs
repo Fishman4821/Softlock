e0f39107d303b0d0aab068995b56a221938a04cd
Redcar005 <williamwoodruff@blazermail.net>
2025-02-17 13:21:21 -0600

    Level 6

---
+++ TSA_Game/Assets/Scripts/LevelSixSceneChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSixSceneChange : MonoBehaviour
+{
+    public void LoadLevelSixScene()
+    {
+        SceneManager.LoadScene("Scenes/LevelSix");
+    }
+}
